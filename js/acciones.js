2603027defbab8e6e468fb200b7b6baf8e8640e8
cesarherrera16 <cesar.herrera.cbtis16@gmail.com>
2017-05-25 15:04:09 -0500

    5subida

--- js/acciones.js
+++ js/acciones.js
@@ -32,7 +32,7 @@ function getPosition(){
 		  'Longitude:'     +position.coords.longitude  +'\n'+
 		  'Altitude:'     +position.coords.altitude  +'\n'+
 		  'Accuracy:'     +position.coords.accuracy  +'\n'+
-		  'Altitude Accuracy:'     +position.coords.atitudeAccuracy  +'\n'+
+		  'Altitude Accuracy:'     +position.coords.altitudeAccuracy  +'\n'+
 	      'Heading:'     +position.coords.heading  +'\n'+
 		  'Speed:'     +position.coords.speed  +'\n'+
 		  'Timestamp:'     +position.timestamp  +'\n');
@@ -62,7 +62,7 @@ $('#accuracy').html(position.coords.accuracy);
 $('#aaccuracy').html(position.coords.altitudeAccuracy);
 $('#headingg').html(position.coords.heading);
 $('#speed').html(position.coords.speed);
-$('#timestamp').html(position.coords.timestamp);
+$('#timestamp').html(position.timestamp);
 	};
 	
 	function onError(error){
